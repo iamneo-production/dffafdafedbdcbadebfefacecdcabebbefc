9abcbb7d72ad13bf7387231f39a549dba359174b
Iamneo <techadmin@iamneo.ai>
2023-08-18 05:16:08 +0000

    Backup Commit

--- springapp/src/main/main/java/com/examly/springapp/Client.java
+++
@@ -1,5 +0,0 @@
-package com.examly.springapp;
-
-public interface Client {
-    
-}
--- springapp/src/main/main/java/com/examly/springapp/Service.java
+++
@@ -1,8 +0,0 @@
-package com.examly.springapp;
-
-import java.util.List;
-
-public interface Service {
-	public List<Cricketer> getAllCricketer();
-	public void addCricketer(Cricketer cricketer);
-}
